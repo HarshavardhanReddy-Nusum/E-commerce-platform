a87a751fc03c76075a5eab9ec6753474871155d2
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2026-08-03 21:36:08 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = require('./src/app')
 const connectDb = require('./src/db/db')
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Running on the server 3000");
 })
 
